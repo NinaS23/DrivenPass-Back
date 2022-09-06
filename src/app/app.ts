84c66c351b3f7f12fc84630d74bc890af6d00575
MariaDuda <mariabevenuto062@gmail.com>
2022-09-06 12:47:26 -0300

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,21 @@
+import express, {json} from "express";
+import "express-async-errors";
+import cors from "cors";
+import dotenv from "dotenv";
+/* import errorHandler from "./middlewares/errorHandler.js";
+*/
+import router from "../routes/index.js"
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+
+app.use(router);
+/* 
+app.use(errorHandler); */
+
+export default app;
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,16 @@
+import httpStatus from "../utils/httpStatus.js";
+import { NextFunction, Request, Response } from "express";
+
+export default async function errorHandler(error, req: Request, res: Response, next: NextFunction) {
+   if(error.code === "not-found"){
+    return res.status(httpStatus.NOT_FOUND).send(error.message)
+   }
+   if(error.code === "unauthorized"){
+    return res.status(httpStatus.UNAUTHORIZED).send(error.message)
+   }
+   if(error.code === "no-content"){
+    return res.status(httpStatus.NO_CONTENT).send(error.message)
+   }
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR)
+    
+  }
