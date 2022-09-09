30ed7b931ba31ff2c7cde9c1410efa43b2f40745
MariaDuda <mariabevenuto062@gmail.com>
2022-09-09 01:42:14 -0300

    feat card schema

--- src/schemas/cardSchema.ts
+++ src/schemas/cardSchema.ts
@@ -1,9 +1,15 @@
 import joi from "joi";
+import DateExtension from '@joi/date';
+
+const Joi = joi.extend(DateExtension);
 
 export const cardSchema = joi.object({
-    name:joi.string().required(),
-    title:joi.string().required(),
-    password:joi.string().required(),
-    isVirtual:joi.string().valid("yes","no").required(),
-    type: joi.string().valid("credit","debit"," debitAndCredit").required()
+    card_number: joi.string().min(15).max(16).required(),
+    name: joi.string().required(),
+    title: joi.string().required(),
+    CVC: joi.string().min(3).max(3).required(),
+    expirationDate: Joi.date().format('YYYY-MM-DD').raw().required(),
+    password: joi.string().required(),
+    isVirtual: joi.string().valid("yes", "no").required(),
+    type: joi.string().valid("credit", "debit", " debitAndCredit").required()
 })
